3e823c629aa949f5404a1f93be365c7d27e85575
iuscl-ide <iuscl.ide@gmail.com>
2019-05-15 18:03:34 +0300

    Generation

--- static-catalog-generator/dist/static-catalog-dev/_catalog/scripts/static-catalog.js
+++ static-catalog-generator/dist/static-catalog-dev/_catalog/scripts/static-catalog.js
@@ -16,7 +16,7 @@ const StaticCatalog = (() => {
 	var indexLinesModulo;
 
 	/** debug */
-	var isDebug = true
+	var isDebug = true;
 	
 	/** debug console */
 	const c = (message, object) => {
@@ -26,7 +26,15 @@ const StaticCatalog = (() => {
 			console.log(object);
 		}
 	}
-	
+
+	/** debug console */
+	const cl = () => {
+		
+		if (isDebug) {
+			console.clear();
+		}
+	}
+
 	/** Initialize */
 	const _initialize = () => {
 		
@@ -50,56 +58,7 @@ const StaticCatalog = (() => {
 		xmlHttpRequest.send();
 	};
 	
-	/** https://gist.github.com/lovasoa/3361645 */
-	function array_intersect() {
-		var i, all, shortest, nShortest, n, len, ret = [], obj = {}, nOthers;
-		nOthers = arguments.length - 1;
-		nShortest = arguments[0].length;
-		shortest = 0;
-		for (i = 0; i <= nOthers; i++) {
-			n = arguments[i].length;
-			if (n < nShortest) {
-				shortest = i;
-				nShortest = n;
-			}
-		}
-
-		for (i = 0; i <= nOthers; i++) {
-			n = (i === shortest) ? 0 : (i || shortest); // Read the shortest array
-														// first. Read the first
-														// array instead of the
-														// shortest
-			len = arguments[n].length;
-			for (var j = 0; j < len; j++) {
-				var elem = arguments[n][j];
-				if (obj[elem] === i - 1) {
-					if (i === nOthers) {
-						ret.push(elem);
-						obj[elem] = 0;
-					} else {
-						obj[elem] = i;
-					}
-				} else if (i === 0) {
-					obj[elem] = 0;
-				}
-			}
-		}
-		return ret;
-	}
-
-//	/** Union */
-//	const _indexArraysUnion = (arr1, arr2) => {
-//		
-//		arr1.push()
-//	    const cache = {};
-//
-//	    a.forEach(item => cache[item] = item);
-//	    b.forEach(item => cache[item] = item);
-//
-//	    return Object.keys(cache).map(key => cache[key]);
-//	};
-
-
+	/** */
 	const _loadBlock = (searchFilters, searchBlocks, blockIndex, resultLines, resultsCallback) => {
 		
 		let index = searchBlocks[blockIndex];
@@ -326,17 +285,93 @@ const StaticCatalog = (() => {
 		}
 		_addUntil(currentArray, sortedUnion, currentIndex, Number.MAX_SAFE_INTEGER);
 		
-		c("before", sortedUnion);
+//		c("before", sortedUnion);
 		
 		sortedUnion = _compactLines(sortedUnion);
 
-		c("after", sortedUnion);
+//		c("after", sortedUnion);
 
 		return sortedUnion;
 	}
 
+	/** Intersection */
+	const _createIntervalIntersection = (line1, line2) => {
+		
+		let startLine1 = _getStartLine(line1);
+		let endLine1 = _getEndLine(line1);
+
+		let startLine2 = _getStartLine(line2);
+		let endLine2 = _getEndLine(line2);
+
+		if (startLine1 === 0) {
+			if (startLine2 === 0) {
+				return (endLine1 === endLine2) ? endLine1 : null;
+			}
+			else {
+				return ((startLine2 <= endLine1) && (endLine1 <= endLine2)) ? endLine1 : null;
+			}
+		}
+		else {
+			if (startLine2 === 0) {
+				return ((startLine1 <= endLine2) && (endLine2 <= endLine1)) ? endLine2 : null;
+			}
+			else {
+				if ((startLine2 > endLine1) || (startLine1 > endLine2)) {
+					return null;
+				}
+				else {
+					let startLine = (startLine1 >= startLine2) ? startLine1 : startLine2;
+					let endLine = (endLine1 <= endLine2) ? endLine1 : endLine2;
+					return (startLine === endLine) ? startLine : startLine * indexLinesModulo + endLine;
+				}
+			}
+		}
+	};
+
+	/** Intersection */
+	const _createIntersection = (array1, array2) => {
+		
+		let index1 = 0;
+		let index2 = 0;
+		let array1Length = array1.length;
+		let array2Length = array2.length;
+		
+		let intersection = [];
+		
+		while ((index1 < array1Length) && (index2 < array2Length)) {
+			let line1 = array1[index1];
+//			c("index1", index1);
+			while (index2 < array2Length) {
+//				c("index2", index2);
+				let line2 = array2[index2];
+				let line = _createIntervalIntersection(line1, line2);
+				if (line === null) {
+					if (_getLine(line1) > _getEndLine(line2)) {
+						index2++;
+					}
+					else {
+						index1++;
+						break;
+					}
+				}
+				else {
+					intersection.push(line);
+					if (_getEndLine(line1) >= _getEndLine(line2)) {
+						index2++;	
+					}
+					else {
+						index1++;
+						break;
+					}
+				}
+			}
+		}
+		
+		return intersection;
+	};
+
 	/** Load an index name */
-	const _loadIndex = (indexTypeFiles, indexTypeFileIndex, searchData, indexValues, resultsCallback) => {
+	const _loadIndex = (indexTypeFiles, indexTypeFileIndex, searchData, indexLines, loadIndexResolve) => {
 		
 //		let p = new Promise( (resolve, reject) => {
 //			c("Promise", "");
@@ -345,20 +380,7 @@ const StaticCatalog = (() => {
 //			c("Then", "");
 //		});
 		
-//		let indexFilePromises = [];
 		let indexFiles = indexTypeFiles[indexTypeFileIndex];
-//		for (let indexFile of indexFiles) {
-//			let indexFilePromise = (indexFile) => {
-//				
-//				return new Promise( (resolve, reject) => {
-//					
-//					c("indexFile", indexFile);
-//					//resolve();
-//				});
-//			};
-//			indexFilePromises.push(indexFilePromise);
-//		};
-
 		let indexNameValuesLines = [];
 		let indexFilePromises = indexFiles.map( (indexFile) => {
 
@@ -369,7 +391,7 @@ const StaticCatalog = (() => {
 					if ((xmlHttpRequest.readyState == 4) && (xmlHttpRequest.status == 200)) {
 						
 						let lines = JSON.parse(xmlHttpRequest.responseText);
-						c("indexFile " + indexFile, lines);
+//						c("indexFile " + indexFile, lines);
 						indexNameValuesLines[indexFiles.indexOf(indexFile)] = lines; 
 						resolve();
 					}
@@ -382,10 +404,10 @@ const StaticCatalog = (() => {
 		
 	    Promise.all(indexFilePromises).then( () => {
 	    	
-//	    	c("All lines for name index finished downloading", "");
+//	    	c("All lines for name index finished downloading", indexTypeFileIndex);
 	    	let indexNameLines = [];
 	    	for (let indexNameValuesLine of indexNameValuesLines) {
-	    		c("indexNameValuesLine", indexNameValuesLine);
+//	    		c("indexNameValuesLine", indexNameValuesLine);
 	    		if (indexNameLines.length === 0) {
 	    			indexNameLines = indexNameValuesLine;
 	    		}
@@ -393,105 +415,39 @@ const StaticCatalog = (() => {
 	    			indexNameLines = _createSortedUnion(indexNameValuesLine, indexNameLines);
 	    		}
 	    	}
-	    	c("indexNameLines", indexNameLines);
+	    	c("union indexNameLines", indexNameLines);
+	    	
+	    	if (indexLines === null) {
+	    		indexLines = indexNameLines;
+	    	}
+	    	else {
+	    		indexLines = _createIntersection(indexLines, indexNameLines);
+	    		if (indexLines.length === 0) {
+	    			c("Intersection empty, exit", indexLines);
+	    			loadIndexResolve(indexLines);
+	    			return;
+	    		}
+	    		else {
+	    			c("intersection", indexLines);
+	    		}
+	    	}
+	    	
+	    	indexTypeFileIndex++;
+	    	if (indexTypeFileIndex < indexTypeFiles.length) {
+	    		_loadIndex(indexTypeFiles, indexTypeFileIndex, searchData, indexLines, loadIndexResolve);
+	    	}
+	    	else {
+	    		c("all files downloaded and union", "");
+	    		loadIndexResolve(indexLines);
+	    	}
 	    });
-		
-		
-//		const xmlHttpRequest = new XMLHttpRequest();
-//		xmlHttpRequest.onreadystatechange = () => {
-//			if ((xmlHttpRequest.readyState == 4) && (xmlHttpRequest.status == 200)) {
-//				
-//				const jsonString = xmlHttpRequest.responseText;
-//				const results = JSON.parse(jsonString);
-//				
-//				console.log(results);
-//				
-//				let indexFieldValues = [];
-//				for (value of searchData.searchFieldsValues[indexIndex].values) {
-//					console.log(value);
-//					indexFieldValues = indexFieldValues.concat(results[value]);
-//				}
-//				if (indexIndex === 0) {
-//					indexValues = indexFieldValues;
-//				}
-//				else {
-//					indexValues = array_intersect(indexValues, indexFieldValues);
-//				}
-//				
-//				//indexValues[indexIndex] = indexFieldValues;
-//				console.log(indexValues);
-//
-//				indexIndex = indexIndex + 1;
-//				if (indexIndex < indexFiles.length) {
-//					_loadIndex(indexFiles, indexIndex, searchData, indexValues, resultsCallback);
-//				}
-//				else {
-//					
-//					
-//				}
-	
-				
-//				for (let result of results.data) {
-//
-//					let ok = true;
-//					for (searchFilter of searchFilters) {
-//						let searchFieldName = searchFilter.field;
-//						resultValue = result[searchFieldName];
-//						if (!searchFilter.values.includes(resultValue)) {
-//							ok = false;
-//							break;
-//						}
-//					}
-//					if (ok) {
-//						resultLines.push(result);
-//					}
-//				}
-//				
-//				if (resultLines.length > 9) {
-//					console.log("Found in block: " + blockIndex + " -> " + index);
-//					//console.log(resultLines);
-//					resultsCallback(resultLines.slice(0, 10));
-//				}
-//				else {
-//					blockIndex++;
-//					if (blockIndex >= searchBlocks.length) {
-//						console.log("No more blocks");
-//						resultsCallback(resultLines);
-//					}
-//					else {
-//						_loadBlock(searchFilters, searchBlocks, blockIndex, resultLines, resultsCallback);	
-//					}
-//				}
-				
-				//resultsCallback(results);
-				
-//				console.log(results);
-//				
-//				var index = 0;
-//				var concat = "";
-//				for (let result of results.data) {
-//					concat = concat + index + " " + results.data[index][1] + "\n";
-//					index++;
-//					
-//				}
-				
-				//console.log(concat);
-				
-//				console.log("done");
-				//document.getElementById("demo").innerHTML = this.responseText;
-				//console.log(xmlHttpRequest.responseText.slice( 0, 100 ));
-				//console.log(this.responseText);
-				//console.log("apply2");
-//			}
-//		};
-//		xmlHttpRequest.open("GET", indexFiles[indexIndex], true);
-//		xmlHttpRequest.overrideMimeType("text/json");
-//		xmlHttpRequest.send();
 	}
 
 	/** Search received */
 	const applyFilters = (searchData, resultsCallback) => {
 		
+		let startMs = (new Date()).getTime();
+		cl();
 		let searchFilters = searchData.searchFieldsValues;
 		
 		let indexTypeFiles = [];
@@ -509,52 +465,15 @@ const StaticCatalog = (() => {
 		}
 		c("indexTypeFiles", indexTypeFiles);
 		
-		_loadIndex(indexTypeFiles, 0, searchData, [], resultsCallback);
+		var indexLines = null;
+		new Promise( (loadIndexResolve, reject) => {
+
+			_loadIndex(indexTypeFiles, 0, searchData, indexLines, loadIndexResolve);
+		}).then((indexLines) => {
+			
+			c("done in " + ((new Date()).getTime() - startMs), indexLines);
+		});
 		
-//		let nameValuesBlocks = searchData.searchCatalog.nameValuesBlocks;
-//		let searchBlocks;
-//		
-//		noResults:
-//		for (searchFilter of searchFilters) {
-//			
-//			let searchFieldName = searchFilter.field;
-//			//console.log(searchFieldName);
-//			
-//			let valuesBlocks;
-//			for (searchValue of searchFilter.values) {
-//				//console.log(searchValue);
-//
-//				let blocks = nameValuesBlocks[searchFieldName][searchValue];
-//				//console.log(blocks);
-//				
-//				if (!valuesBlocks) {
-//					valuesBlocks = blocks;
-//				}
-//				else {
-//					//valuesBlocks = _union(valuesBlocks, blocks);
-//					valuesBlocks = valuesBlocks.concat(blocks);
-//				}
-//			}
-//			if (!searchBlocks) {
-//				searchBlocks = valuesBlocks;
-//			}
-//			else {
-//				console.log("Start intersection...");
-//				searchBlocks = array_intersect(searchBlocks, valuesBlocks);
-//					
-//					//searchBlocks.filter(value => valuesBlocks.includes(value));
-//				console.log("Done intersection.");
-//				if (searchBlocks.length == 0) {
-//					break noResults;
-//				}
-//			}
-//		}
-//		
-//		console.log("Done apply filters, search blocks:");
-//		console.log(searchBlocks);
-//		
-//		let resultLines = [];
-//		_loadBlock(searchFilters, searchBlocks, 0, resultLines, resultsCallback);
 	}
 
 	/* Constructor */
